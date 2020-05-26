20971e070089faefafa45c71b62f5060303647b1
Alexander Rice <webdev@ricealexander.com>
2020-05-26 15:25:48 -0500

    :tada: Create Working SASS Project
    .browserslistrc configures browser-support for Autoprefixer
    .editorconfig sets basic editor settings
    gruntfile.js sets up build steps

---
+++ README.md
@@ -0,0 +1,2 @@
+# grove-custom-stylesheet
+Custom Styles for Grove CMS
---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = grunt => {
+  grunt.loadNpmTasks('grunt-sass')          // sass
+  grunt.loadNpmTasks('grunt-postcss')       // postcss
+  grunt.loadNpmTasks('grunt-contrib-watch') // watch
+
+  grunt.initConfig({
+    sass: {
+      options: {
+        implementation: require('node-sass'),
+      },
+      dist: {
+        files: { 'dist/index.css': 'sass/index.scss' },
+      },
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')(),
+          require('cssnano')(),
+        ],
+      },
+      dist: {
+        src: 'dist/*.css',
+      },
+    },
+
+    watch: {
+      sass: {
+        files: 'sass/**',
+        tasks: ['clean', 'sass', 'postcss'],
+        options: { livereload: true },
+      },
+    },
+  })
+
+  grunt.registerTask('clean', 'Remove the dist directory', () => {
+    grunt.file.delete('./dist')
+  })
+
+  grunt.registerTask('default', ['clean', 'sass', 'postcss'])
+}
